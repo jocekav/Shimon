7df508dc77bb6a6d641730c93648c57da8d55069
jocekav <jkavanagh3@gatech.edu>
2022-10-07 00:51:23 -0400

    New similarity measures and keyboard input

--- genetic.py
+++ genetic.py
@@ -1,3 +1,4 @@
+from distutils.command import clean
 from queue import Empty
 import random
 from collections import Counter
@@ -7,7 +8,12 @@ import operator
 import math
 import pythonosc
 from pythonosc import udp_client
+from pythonosc import dispatcher
+from pythonosc import osc_server
+from typing import List, Any
 import time
+import datetime
+import keyboard
 
 def percentage_pitch(pattern, pitch_options):
     # create dictionary for avail pitches
@@ -41,6 +47,98 @@ def get_pitch_mct(pattern):
     stdev_pitch = stdev(elem[1] for elem in pattern)
     return (mean_pitch, median_pitch, stdev_pitch)
 
+def calculate_intervals(pattern):
+    first_note = pattern[0][1]
+    intervals = []
+    for i in range(1, len(pattern)):
+        second_note = pattern[i][1]
+        intervals.append(second_note - first_note)
+        first_note = second_note
+    return intervals
+
+# attempt to define contour from front and back arcs (disregards middle if one melody is much longer)
+def check_order_front_back(patt_1, patt_2):
+    front = 0
+    back_1 = len(patt_1) - 1
+    back_2 = len(patt_2) - 1
+    matches_pitch = 0
+    matches_rhythm = 0
+    while(front < back_1 or front < back_2):
+        if (patt_1[front][0] == patt_2[front][0]):
+            matches_rhythm += 1
+        if (patt_1[front][1] == patt_2[front][1]):
+            matches_pitch += 1
+        if (patt_1[back_1][0] == patt_2[back_2][0]):
+            matches_rhythm += 1
+        if (patt_1[back_1][1] == patt_2[back_2][1]):
+            matches_pitch += 1
+        front = front + 1
+        back_1 = back_1 + 1
+        back_2 = back_2 + 1
+    shorter_len = len(patt_1)
+    if len(patt_2) < shorter_len:
+        shorter_len = len(patt_2)
+    percent_pitch = matches_pitch / shorter_len
+    percent_rhythm = matches_rhythm / shorter_len
+    return (percent_rhythm, percent_pitch)
+    
+def step_to_leap_ratio(intervals):
+    # step defined as minor third and below (everything else is a leap)
+    # returns step / leap
+    steps = 0
+    leaps = 0
+    for i in range(len(intervals)):
+        if abs(intervals[i]) > 3:
+            leaps += 1
+        else:
+            step += 1
+    if leaps == 0:
+        return 1
+    else:
+        return steps / leaps
+
+def get_abstracted_contour(pattern):
+    patt_len = len(pattern)
+    min_pitch = min(pattern)[1]
+    max_pitch = max(pattern)[1]
+    first_pitch = pattern[0][1]
+    last_pitch = pattern[patt_len - 1][1]
+    mid_point = int(patt_len / 2)
+    mid_pitch = pattern[mid_point][1]
+
+    if (min_pitch == max_pitch):
+        return 'flat'
+    elif max_pitch == first_pitch:
+        if min_pitch == last_pitch or last_pitch < mid_pitch:
+            return 'descend'
+        elif max_pitch == last_pitch or last_pitch >= mid_pitch:
+            return 'v'
+    elif min_pitch == first_pitch:
+        if max_pitch == last_pitch or last_pitch >= mid_pitch:
+            return 'ascend'
+        elif min_pitch == last_pitch or last_pitch < mid_pitch:
+            return 'arch'
+
+    return 'undef'
+
+def expand_pattern(pattern, rhythm_options):
+    expanded_pattern = []
+    min_unit = min(rhythm_options)
+    for i in range(len(pattern)):
+        rhythm, pitch = pattern[i]
+        num_units = rhythm / min_unit
+        expanded_note = [pitch] * num_units
+        expanded_pattern.extend(expanded_note)
+    return expanded_pattern
+
+def transpose_to_C(pattern):
+    diff = pattern[0][1] - 60
+    transp_patt = []
+    for i in range(len(pattern)):
+        transp_patt.append(pattern[i][1] + diff)
+    return transp_patt
+
+
 class world:
     def __init__(self, target_pattern, population_file, pitch_options, rhythm_options):
         self.pitch_options = pitch_options
@@ -49,7 +147,7 @@ class world:
         self.population = self.build_population(population_file)
         self.population.sort(key = lambda x: x.fitness)
         self.population_size = len(self.population)
-        IP = "143.215.124.77"
+        IP = "143.215.114.123"
         PORT_TO_MAX = 7980
         self.client = udp_client.SimpleUDPClient(IP, PORT_TO_MAX)
     
@@ -73,7 +171,7 @@ class world:
         for i in range(5):
             print(self.population[i].get_fitness(), self.population[i].get_pattern())
 
-    def run(self, num_generations=10, survival_rate=.5, mutation_rate=.3):
+    def run(self, num_generations=10, survival_rate=.3, mutation_rate=.3):
         for i in range(num_generations):
             # check evolution rate
             print("Generation " + str(i-1))
@@ -97,7 +195,7 @@ class world:
             # self.print_population()
             self.play_melodies()
             input("press enter to continue")
-        
+         
 
     def breed(self):
         parent1 = (random.choice(self.population)).get_pattern()
@@ -162,7 +260,7 @@ class world:
                     if not inserted:
                         gene_pattern = orig_gene_pattern
                     gene.reinit(gene_pattern)
-                elif len(gene_pattern) < 64:
+                elif len(gene_pattern) < num_beats / 0.25:
                     inserted = False
                     ind = random.randint(0, (len(gene_pattern)-1))
                     while not inserted:
@@ -267,28 +365,58 @@ class gene:
         # could do rhythm over time
         # could do the probabilities that a note is followed by another note
 
-        # difference in notes and rhythms used (in order)
-        # only uses matching notes of the shorter pattern
-        # TODO: figure out a better way to solve this metric
-        # if target_len <= pattern_len:
-        #     for i in range(target_len):
-        #         # get pitch difference
-        #         euclid_sum += ((target[i][2] - pattern[i][2]) ** 2)
-        #         # get rhythm difference
-        #         euclid_sum += ((target[i][1] - pattern[i][1]) ** 2)
-        # else:
-        #     for i in range(pattern_len):
-        #         # get pitch difference
-        #         euclid_sum += ((target[i][2] - pattern[i][2]) ** 2)
-        #         # get rhythm difference
-        #         euclid_sum += ((target[i][1] - pattern[i][1]) ** 2)
 
-target_patt = [(2, 60), (2, 62), (2, 64), (2, 65), (2, 67), (2, 69), (1, 71), (1, 72), (1, 71), (1, 72)]
+# def clean_max_input(address: str, *args: List[Any]):
+#     print(address)
+#     print(args)
+#     ms = args[0]
+#     pitch = args[1]
+
+#     tempo = 200
+#     dur_in_ms = 60000 / tempo
+#     total_dur = dur_in_ms * num_beats * 4
+#     note_length = ms / total_dur
+#     target_patt.append((note_length, pitch))
+#     print(target_patt)
+    
+
+# def listen2Max(ip,port,path):
+#     '''
+#     set up server
+#     '''
+#     # dispatcher to receive message
+#     disp = dispatcher.Dispatcher()
+#     disp.map(path, clean_max_input)
+#     # server to listen
+#     server = osc_server.ThreadingOSCUDPServer((ip,port), disp)
+#     print("Serving on {}".format(server.server_address))
+#     server.serve_forever()
+#     # server.handle_request()
+#     # server.serve()
+
+# target_patt = [(2, 60), (2, 62), (2, 64), (2, 65), (2, 67), (2, 69), (1, 71), (1, 72), (1, 71), (1, 72)]
+target_patt = [(1, 71), (2, 60), (4, 62), (4, 64), (4, 69), (1, 67)]
 
 # pitch_options = [-1, 60, 62, 64, 65, 67, 69, 71, 72]
 pitch_options = [60, 62, 64, 65, 67, 69, 71, 72]
 num_beats = 16
 rhythm_options = [0.25, 0.5, 1, 1.5, 2, 3, 4]
+
+# target_patt = []
+
+# tempo = 60
+# dur_in_ms = 60000 / tempo
+# total_dur = dur_in_ms * num_beats * 4
+# print(total_dur)
+# IP = "143.215.114.123"
+# R_PORT_TO_MAX = 4980
+# endTime = datetime.datetime.now() + datetime.timedelta(milliseconds=total_dur)
+# while(True): 
+#     listen2Max(IP, R_PORT_TO_MAX, '/max')
+#     # if keyboard.is_pressed("s"):
+#     #     break  
+# print(target_patt)
+
 
 world = world(target_patt, "melodies.txt", pitch_options, rhythm_options)
 world.print_population()
